3ef2ca903d8a7b11cac24f524d31c8138212be35
Amalie Von Behren <vbamalie@gmail.com>
2022-07-15 13:09:47 -0400

    server made

--- server/server.js
+++ server/server.js
@@ -1,1 +1,20 @@
-const express = require("express")
+const express = require("express");
+const  path = require("path");
+const db = ("./config/connection");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
